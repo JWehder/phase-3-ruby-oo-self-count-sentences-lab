be0ebd619e6c2801a912f159c8e76ca4eae4f678
Jake Wehder <jw@email.comjake>
2022-11-28 21:13:29 -0600

    commit tree 2e0d65c7898150f357d85c0b2324ce0d5ef719af

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.!?]+/)
+    sentence_array.count
   end
 end
+
+sentence = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+puts sentence.sentence?
+puts sentence.count_sentences
+print "The weather outside is frightful.".sentence?
